3553684880cebb0eda4b6199aab74ecb10f42fc5
Sam Gochman <sam@sosolimited.com>
2021-01-23 15:03:49 -0800

    combine analyses; start image selection process

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
     <div class="model__container">
+      <h1>1. Upload your full puzzle:</h1>
+      <input id="select-full" type="file" accept="image/png, image/jpeg, image/tiff" onchange="fullImgSrc = window.URL.createObjectURL(this.files[0])">
+      <img id="full-img__img"/>
+      <h1>and your puzzle piece:</h1>
+      <input id="select-sample" type="file" accept="image/png, image/jpeg, image/tiff" onchange="document.getElementById('sample-img__img').src = window.URL.createObjectURL(this.files[0])">
       <div class="img__container">
-        <img id="sample-img__img" src="./assets/sample_kite.jpg"/>
+        <img id="sample-img__img"/>
         <canvas id="sample-img__canvas"></canvas>
       </div>
-      <button type="button" v-on:click="analyzeSample()" style="width: 90px">Run sample</button>
+    </div>
+
+    <div class="model__container">
+      <h1>2. Run the analysis:</h1>
+      <button type="button" v-on:click="analyzeSample()">Find it!</button>
     </div>
     
-    <div class="model__container">
+    <div class="model__container model__container--full-img">
+      <h1>3. Here's where the piece might belong:</h1>
       <div class="img__container">
-        <img id="full-img__img" src="./assets/full_image.jpg"/>
+        <img class="full-img__img"/>
         <canvas id="full-img__canvas"></canvas>
       </div>
-      <button type="button" v-on:click="runModel('full-img__img', 'full-img__canvas', false)" style="width: 80px">Run full</button>
     </div>
+    <button @click="checkFull"></button>
   </div>
 </template>
 
@@ -24,8 +34,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"> </script>
 
 <script>
-import P5 from 'p5';
-
 // Notice there is no 'import' statement for tensorflow or coco-ssd. 'cocoSsd' and 'tf' is
 // available on the index-page because of the script tag above.
 
@@ -33,15 +41,46 @@ export default {
   name: 'App',
   data() {
     return {
-      sampleClass: ''
+      sampleImgSrc: '/assets/sample_kite.jpg',
+      sampleClass: '',
+      fullImgSrc: '/assets/full_image.jpg'
     }
   },
   mounted() {},
+  // computed: {
+  //   sampleImgScr() {
+  //     return '/assets/sample_kite.jpg';
+  //   }
+  // },
   methods: {
+    checkFull() {
+      console.log(this.fullImgSrc);
+    },
+    selectInput(input) {
+      // document.getElementById('full-img__img').src = window.URL.createObjectURL(input.files[0])
+      console.log(input);
+    },
+    readURL(input) {
+      console.log(input);
+      const vm = this;
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function () {
+          vm.sampleImgSrc = reader.result;
+          document.getElementById('sample-img__img')
+          // $('#blah')
+          //   .attr('src', e.target.result)
+          //   .width(150)
+          //   .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
     async analyzeSample() {
       const vm = this;
-      // this.runModel('sample-img__img', 'sample-img__canvas', true).then(cls => {
-      vm.sampleClass = await this.runModel('sample-img__img', 'sample-img__canvas', true);
+      vm.sampleClass = await this.runModel('sample-img__img', 'sample-img__canvas', true);  // run model on sample to retrieve object class
+      await this.runModel('full-img__img', 'full-img__canvas', false);  // run model on full image to identify locations of same object class
     },
     runModel(imgId, canvasId, isSample) {
       console.log('Running model...');
@@ -94,6 +133,10 @@ export default {
 </script>
 
 <style>
+html {
+  /* background-color: rgb(192, 217, 241); */
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -102,19 +145,34 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   display: flex;
+  /* flex-direction: column; */
+  /* align-items: center; */
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-around;
 }
 
 .model__container {
   display: flex;
-  flex-direction: column;
+  /* flex-direction: row;
+  justify-content: flex-start; */
   align-items: center;
+  /* width: 1024px; */
+  width: 700px;
+  /* margin-bottom: 50px; */
+  flex-direction: column;
+  border: 1px solid #2c3e50;
+  border-radius: 25px;
 }
+
+/* .model__container.model__container--full-img {
+  flex-direction: column;
+  align-items: flex-start;
+} */
 
 .img__container {
   position: relative;
   display: inline-block;
+  width: min-content;
 }
 
 #sample-img__canvas,
@@ -124,5 +182,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
+}
+
+button {
+  width: min-content;
 }
 </style>
